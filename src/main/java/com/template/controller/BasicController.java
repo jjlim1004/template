74704fe7134df22aef49b2f0a5508e06a331165c
jjim1004 <jjlim1004@naver.com>
2025-01-23 23:52:41 +0900

    feat: spring Template first step

---
+++ src/main/java/com/template/controller/BasicController.java
@@ -0,0 +1,14 @@
+package com.template.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+public class BasicController {
+    @RequestMapping("/welcome" )
+    @ResponseBody
+    public String welcome(){
+        return"welcome";
+    }
+}
