2208d5d45797cc22fc3057e54dae6938d628e65d
jjim1004 <jjlim1004@naver.com>
2025-01-24 23:33:14 +0900

    feat: spring Template -jsp view resolver test

--- src/main/java/com/template/controller/BasicController.java
+++ src/main/java/com/template/controller/BasicController.java
@@ -1,6 +1,7 @@
 package com.template.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
@@ -10,5 +11,16 @@ public class BasicController {
     @ResponseBody
     public String welcome(){
         return"welcome";
+    }
+
+    @RequestMapping("/welcome-view" )
+    public String welcomeView(){
+        return"welcome";
+    }
+
+    @RequestMapping("/welcome-model-view" )
+    public String welcome(ModelMap model){
+        model.put("name","this it the name");
+        return"welcome-model-map";
     }
 }
