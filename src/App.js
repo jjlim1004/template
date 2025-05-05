b9b57b841cc725e48f869c3ef99f5f8993093ffb
jjim1004 <jjlim1004@naver.com>
2025-05-05 22:03:29 +0900

    study: 리액트 jsx 실습
    왠만한건 다 중괄호가 된다.

--- src/App.js
+++ src/App.js
@@ -2,23 +2,23 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
+    let posts = 'nyang nyang';
+    let className = 'black-nav';
+    let inlineClass = {color:'gray', fontSize: '30px'}
+    function sampleFx(){
+        return 100;
+    }
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+      <div className="App">
+          <div className={className}>
+              <div style={inlineClass}>
+                  react develop blog
+              </div>
+          </div>
+          <img src={logo}/>
+          <h4>{posts}</h4>
+          <h4>{sampleFx()}</h4>
+      </div>
   );
 }
 
